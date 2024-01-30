7c388ce4b948822aa2f9e8e4aa8e2c52832da38f
ameya-6964 <belvalkar7@gmail.com>
2024-01-31 00:49:05 +0530

    Understood Working Of Props

--- 01-tailwind-and-props/src/App.jsx
+++ 01-tailwind-and-props/src/App.jsx
@@ -1,5 +1,14 @@
+import Card from "./components/Card";
+
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-wrap justify-between gap-3 p-2">
+      <Card name={"Shruti"} role={"Full Stack Web Developer"} />
+      <Card name={"Shivani"} role={"React.js Developer"} />
+      <Card name={"Samruddhi"} role={"Backend Node.js Web Developer"} />
+      <Card name={"Shreya"} role={"Full Stack Java Developer"} />
+    </div>
+  );
 }
 
 export default App;
