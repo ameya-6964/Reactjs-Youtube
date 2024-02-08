f570cdcc9967fc2dec95d12e93135145f656502b
ameya-6964 <belvalkar7@gmail.com>
2024-02-09 00:17:28 +0530

    Added Basic Functionality

--- 03-password-generator/src/App.jsx
+++ 03-password-generator/src/App.jsx
@@ -1,13 +1,50 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import Input from "./components/Input";
 import "./index.css";
 
 function App() {
+  const [password, setPassword] = useState("");
   const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*-_+=[]{}~`";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+
+  useEffect(
+    function () {
+      passwordGenerator();
+    },
+    [passwordGenerator]
+  );
+
   return (
-    <div className="w-[800px] h-[120px] bg-blue-950 ">
-      <Input />
-    </div>
+    <>
+      <h1 className="text-center mt-5 text-5xl font-bold text-white">
+        Password Generator
+      </h1>
+      <div className="w-[800px] h-[200px] flex flex-col justify-center items-center mt-[40px] bg-gray-800 ">
+        <Input
+          password={password}
+          length={length}
+          numberAllowed={numberAllowed}
+          characterAllowed={characterAllowed}
+          setLength={setLength}
+          setCharacterAllowed={setCharacterAllowed}
+          setNumberAllowed={setNumberAllowed}
+        />
+      </div>
+    </>
   );
 }
 
--- 03-password-generator/src/components/Input.jsx
+++ 03-password-generator/src/components/Input.jsx
@@ -1,14 +1,67 @@
-const Input = () => {
+const Input = ({
+  password,
+  length,
+  numberAllowed,
+  characterAllowed,
+  setLength,
+  setCharacterAllowed,
+  setNumberAllowed,
+}) => {
   return (
-    <div className=" mt-5 h-[50px] flex justify-center items-center">
-      <input
-        type="text"
-        className="w-[450px] h-[35px] rounded-l-md text-orange-400 "
-      />
-      <button className="text-white h-[35px] bg-blue-600 text-center w-auto pt-0 pb-0 pl-3 pr-4 rounded-r-md">
-        Copy
-      </button>
-    </div>
+    <>
+      <div className=" mt-5 h-[50px] flex justify-center items-center">
+        <input
+          type="text"
+          className="w-[450px] h-[35px] rounded-l-md text-black text-lg"
+          value={password}
+          readOnly
+        />
+        <button className="text-white h-[35px] bg-blue-600 text-center w-auto pt-0 pb-0 pl-3 pr-4 rounded-r-md">
+          Copy
+        </button>
+      </div>
+      <div className="flex  h-[60px] items-center justify-center text-orange-400 pt-0 pb-0 gap-5  text-lg mt-3">
+        <div>
+          <input
+            type="range"
+            min={8}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label className="ml-2">Length: {length}</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label className="ml-2" htmlFor="numberInput">
+            Numbers
+          </label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            defaultChecked={characterAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharacterAllowed((prev) => !prev);
+            }}
+          />
+          <label className="ml-2" htmlFor="characterInput">
+            Characters
+          </label>
+        </div>
+      </div>
+    </>
   );
 };
 
